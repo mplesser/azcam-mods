09f706659e04e0b19de0398b07d24b9fa48be198
Michael Lesser <mlesser@arizona.edu>
2024-11-22 09:42:34 -0700

    update

--- azcam_mods/detector_mods.py
+++ azcam_mods/detector_mods.py
@@ -5,8 +5,8 @@ MODS focal plane
 detector_mods = {
     "name": "MODS",
     "description": "MODS",
-    "ref_pixel": [4080.0, 4080.0],
-    "format": [8192, 4, 0, 48, 3072, 8, 0, 0, 0],
+    "ref_pixel": [4096.0, 1536.0],
+    "format": [8192, 4, 0, 0, 3072, 8, 0, 0, 0],
     "focalplane": [1, 1, 2, 2, [0, 1, 0, 1]],
     "roi": [1, 8192, 1, 3072, 1, 1],
     "jpg_order": [1, 2, 3, 4],
