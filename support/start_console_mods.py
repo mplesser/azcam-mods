cbc1340e9835ee6304ceaa612a666fd430525f0a
Michael Lesser <mlesser@arizona.edu>
2025-12-01 08:39:10 -0700

    cleanup

--- support/start_console_mods.py
+++ support/start_console_mods.py
@@ -6,7 +6,7 @@ import subprocess
 
 OPTIONS = "-lab"
 # CMD = f"ipython --ipython-dir=~/data/ipython --profile azcamconsole -i -m azcam_mods.console -- {OPTIONS}"
-CMD = f"ipython --ipython-dir=/data/ipython --profile azcamconsole_mods -i -m azcam_mods.console -- {OPTIONS}"
+CMD = f"ipython --ipython-dir=/data/ipython --profile azcamconsole -i -m azcam_mods.console -- {OPTIONS}"
 
 p = subprocess.Popen(
     CMD,
